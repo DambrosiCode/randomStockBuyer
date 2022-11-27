55520d37f8e947e2956be09465597f01d144f5f2
Matt <tfp221b@gmail.com>
2022-11-26 22:17:35 -0500

    stocks points to new object

--- RandomTrades.py
+++ RandomTrades.py
@@ -3,19 +3,22 @@ from tqdm import tqdm
 from random import randint
 from datetime import timedelta, datetime
 import numpy
+import copy
 
-def randStockBuyer(starting_funds, stocks, date_range, iter, min_stock_move, max_stock_move, days_run=30):
+def randStockBuyer(starting_funds, starting_stocks, date_range, iter, min_stock_move, max_stock_move, days_run=30):
     #randomly buy and sell stock for one month 100 times
     funds = []
     for i in tqdm(range(0,iter-1)):
+        stocks = starting_stocks.copy() #copy the starting amount of stocks again
         trader = Trader(starting_funds,
                         stocks,
                         date_range,
                         redown=False)
-        print(trader)
+        print(stocks)
         # start trader out with one stock: ABC, and no shares (0)
         # only buy shares between 2020 and today
         for day in range(0,days_run):
+
             #buy between 1 and 10 stocks randomly
             stocks_to_move = randint(min_stock_move,max_stock_move)
             buy_or_sell = randint(0,2)
@@ -40,15 +43,17 @@ def randStockBuyer(starting_funds, stocks, date_range, iter, min_stock_move, max
     print('---')
     print(average_rand)
     print(str(start_price) +' '+ str(end_price))
-    return funds
 #
 randStockBuyer(starting_funds=1000,
-                       stocks={'ABC':0},
+                       starting_stocks={'ABC':0},
                        date_range=[datetime(2020, 1, 3), datetime(2022, 11, 21)],
                        iter=9,
                        min_stock_move=1,
                        max_stock_move=10,
                        days_run=30)
+
+
+
 import matplotlib.pyplot as plt
 #plt.hist(funds)
 #plt.show()
